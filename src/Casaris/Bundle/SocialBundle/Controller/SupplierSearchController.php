d2012a078e41520344ed9de71d501b7147111617
Andressa Guimarães <desa.lopes7@gmail.com>
2015-02-15 22:33:22 -0200

    Layout das páginas > Busca > Busca de Fornecedores
    Signed-off-by: Andressa Guimarães <desa.lopes7@gmail.com>

---
+++ src/Casaris/Bundle/SocialBundle/Controller/SupplierSearchController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Casaris\Bundle\SocialBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class SupplierSearchController extends Controller
+{
+    /**
+     * @Route("/supplier")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}

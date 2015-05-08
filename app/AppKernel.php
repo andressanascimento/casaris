458cc7c911c047d01dbdd0da697523fd64ea7dd2
Andressa Guimarães <desa.lopes7@gmail.com>
2015-05-07 21:25:02 -0300

    Adicionar o Bundle de doctrine fixture
    Signed-off-by: Andressa Guimarães <desa.lopes7@gmail.com>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Casaris\Bundle\SocialBundle\SocialBundle(),
             new Casaris\Bundle\CoreBundle\CoreBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
